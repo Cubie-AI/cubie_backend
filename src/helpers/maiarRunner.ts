15ad8f305438f5d0e58686fbb438915a5cde0a74
SolSoc <socrates.nft@gmail.com>
2025-02-17 19:03:23 -0800

    Solana changes

--- src/helpers/maiarRunner.ts
+++ src/helpers/maiarRunner.ts
@@ -2,9 +2,13 @@ import { MAIAR_RUNNER_SERVICE } from "../utils/constants.js";
 import { logger } from "../utils/logger.js";
 
 export async function startAgentRunner(agentId: number) {
+  logger.info(`Starting agent runner for agent ${agentId}`);
   // ping the runner service to start the agent
   const callBackend = await fetch(MAIAR_RUNNER_SERVICE, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({
       agentId,
     }),
