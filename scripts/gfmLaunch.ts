d4eccc5ecdf5a8da012baab598638bcd082df347
SolSoc <socrates.nft@gmail.com>
2025-02-23 05:57:31 -0800

    Add launch on GFM

---
+++ scripts/gfmLaunch.ts
@@ -0,0 +1,160 @@
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { readFileSync } from "fs";
+
+const gfmProgram = new PublicKey(
+  "GFMioXjhuDWMEBtuaoaDPJFPEnL2yDHCWKoVPhj1MeA7"
+);
+
+interface GFMRequest {
+  raise: {
+    claimWindowInHours: number;
+    target: number;
+  };
+  kycConfig: { type: "none" };
+  walletAddress: string;
+  amountIn: number; // ui amount of solana to launch with
+  priorFee: number; // decimal priority fee
+  token: {
+    base64: string; // base64 encoded buffer of image
+    description: string; // token description
+    discord: string; // discord link
+    name: string; // token name
+    symbol: string; // token symbol
+    telegram: string; // telegram link
+    twitter: string; // twitter link
+    website: string; // website link
+  };
+  tokenomics: {
+    decimals: number; // token decimals
+    supply: number; // token supply
+    curveConstant: number; // bonding curve constant
+    curveExponent: number; // bonding curve exponent
+  };
+}
+const endpoint =
+  "https://www.gofundmeme.io/gofundmeme/v1/api/interface/pool/bonding-curve/create/request";
+
+interface MakeGFMParams {
+  image: Buffer;
+  walletAddress: string;
+  name: string;
+  symbol: string;
+  description?: string;
+  amountIn: number;
+  priorFee: number;
+  discord?: string;
+  telegram?: string;
+  twitter?: string;
+  website?: string;
+}
+function makeGFMRequest(params: MakeGFMParams): GFMRequest {
+  const {
+    image,
+    walletAddress,
+    description,
+    name,
+    symbol,
+    amountIn,
+    priorFee,
+    discord,
+    telegram,
+    twitter,
+    website,
+  } = params;
+
+  if (
+    !name ||
+    !image ||
+    !walletAddress ||
+    !symbol ||
+    !amountIn ||
+    isNaN(amountIn)
+  ) {
+    throw new Error("Invalid params");
+  }
+
+  const _priorFee = priorFee || 0.01;
+  return {
+    raise: {
+      claimWindowInHours: 2,
+      target: 0,
+    },
+    kycConfig: { type: "none" },
+    walletAddress,
+    amountIn,
+    priorFee: _priorFee,
+    token: {
+      base64: image.toString("base64"),
+      description: description || "",
+      discord: discord || "",
+      name,
+      symbol,
+      telegram: telegram || "",
+      twitter: twitter || "",
+      website: website || "",
+    },
+    tokenomics: {
+      decimals: 9,
+      supply: 1000000000,
+      curveConstant: 1,
+      curveExponent: 1,
+    },
+  };
+}
+
+type GFMSuccessResponse = {
+  mintAddress: string;
+  requestId: string;
+  rawTransaction: Buffer;
+};
+
+type GFMErrorResponse = {
+  title: string;
+  description: string;
+};
+type GFMResponse =
+  | {
+      success: true;
+      data: GFMSuccessResponse;
+    }
+  | {
+      success: false;
+      data: GFMErrorResponse;
+    };
+async function launch() {
+  const image = readFileSync(import.meta.dirname + "/test.png");
+
+  const params = {
+    image,
+    walletAddress: Keypair.generate().publicKey.toBase58(),
+    name: "Cubie",
+    symbol: "CubieGFM",
+    description: "Testing launching on gfm for cubie.fun",
+    amountIn: 0.15,
+    priorFee: 0.01,
+    telegram: "https://t.me/cubie_portal",
+    twitter: "https://x.com/cubieai",
+    website: "https://cubie.fun",
+  };
+
+  const request = await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify(makeGFMRequest(params)),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const response = (await request.json()) as GFMResponse;
+  console.log(response);
+
+  if (!response.success) {
+    throw new Error(response.data.description);
+  }
+
+  console.log("Success", response.data);
+  // return the response.data.rawTransaction to the user and sign it with the wallet
+  return response.data;
+}
+
+launch();
--- scripts/pumpSubscriber.ts
+++ scripts/pumpSubscriber.ts
@@ -1,18 +1,78 @@
-import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-import { PUMPFUN_PROGRAM } from "../src/utils/constants.js";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { solanaConnection } from "../src/solana/connection.js";
-import { struct } from "@solana/buffer-layout";
+import { PUMPFUN_PROGRAM } from "../src/utils/constants.js";
 // @ts-ignore
-import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
-import bs58 from "bs58";
-import { buy } from "./pumpfun.js";
 const pfProgramId = new PublicKey(PUMPFUN_PROGRAM);
 
-const testMint = new PublicKey("5rHjqLvPWbJQTcGusUJ69sfAaDN8RVzRBR5usX1415cv");
-// Create provider for agent and add to plugin config
-const owner = Keypair.fromSecretKey(
-  bs58.decode(process.env.AGENT_PRIVATE_KEY || "")
-);
-const provider = new AnchorProvider(solanaConnection, new Wallet(owner));
+// const testMint = new PublicKey("5rHjqLvPWbJQTcGusUJ69sfAaDN8RVzRBR5usX1415cv");
+// // Create provider for agent and add to plugin config
+// const owner = Keypair.fromSecretKey(
+//   bs58.decode(process.env.AGENT_PRIVATE_KEY || "")
+// );
+// const provider = new AnchorProvider(solanaConnection, new Wallet(owner));
 
-buy(provider, owner, testMint.toBase58(), 1_000_000);
+// buy(provider, owner, testMint.toBase58(), 1_000_000);
+
+// const provider = new AnchorProvider(
+//   solanaConnection,
+//   new Wallet(Keypair.generate())
+// );
+
+// const program = new Program(IDL as PumpFun, provider);
+
+const SELL_DISCRIMINATOR = Buffer.from([51, 230, 133, 164, 1, 127, 131, 173]);
+const BUY_DISCRIMINATOR = Buffer.from([102, 6, 61, 18, 1, 218, 235, 234]);
+const CREATE_DISCRIMINTAOR = Buffer.from([24, 30, 200, 40, 5, 28, 7, 119]);
+
+async function parsePumpTransaction(signature: string) {
+  const time = Date.now();
+
+  const transaction = await solanaConnection.getTransaction(signature, {
+    maxSupportedTransactionVersion: 0,
+    commitment: "confirmed",
+  });
+  const transactionTime = transaction?.blockTime;
+  // console.dir(transaction, { depth: null });
+
+  // transaction?.meta?.preTokenBalances; => structured objects {accountIndex: number;
+  // mint: string;
+  // owner?: string;
+  // programId?: string;
+  // uiTokenAmount: TokenAmount;}
+  // transaction?.meta?.preBalances = Array<numbers> ==> index corresponds to the account in transaction.message.accountKeys (PublickKey[])
+
+  const pumpfunIndex =
+    transaction?.transaction.message.staticAccountKeys.findIndex((account) =>
+      account.equals(pfProgramId)
+    );
+
+  // console.log(`pumpfun index: ${pumpfunIndex}`);
+  const compiledInsturctions =
+    transaction?.transaction.message.compiledInstructions.filter(
+      (ins) => ins.programIdIndex === pumpfunIndex
+    );
+  // console.dir(compiledInsturctions, { depth: null });
+  compiledInsturctions?.forEach((compiledInstruction) => {
+    const buffer = Buffer.from(compiledInstruction.data);
+
+    const discrim = buffer.readBigUInt64LE(0);
+    const tokenAmount = buffer.readBigUInt64LE(8);
+    const solAmount = buffer.readBigUInt64LE(16);
+
+    const baseData = {
+      type:
+        discrim.toString() === SELL_DISCRIMINATOR.readBigUint64LE().toString()
+          ? "sell"
+          : "buy",
+      signature,
+      tokenAmount: Number(tokenAmount) / Math.pow(10, 6),
+      solAmount: Number(solAmount) / LAMPORTS_PER_SOL,
+    };
+
+    console.dir(baseData, { depth: null });
+  });
+}
+solanaConnection.onLogs(
+  pfProgramId,
+  (data) => !data.err && parsePumpTransaction(data.signature)
+);
